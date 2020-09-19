9696f7dd608c6e4b15afd5af98709c8a5a65db03
Gregory Pevnev <grygory.pevnev@tradomatic.io>
2020-09-19 18:50:30 +0300

    App Engine Refactoring

--- config/default.js
+++ config/default.js
@@ -1,6 +1,15 @@
 const path = require('path'); // eslint-disable-line
 
-if(!process.env.CLOUD_PROJECT_ID|| !process.env.CLOUD_CONVERT_KEY || !process.env.CLOUD_STORAGE_BUCKET)
+const REQUIRED_VARIABLES = [
+  'CLOUD_PROJECT_ID',
+  'CLOUD_CONVERT_KEY',
+  'CLOUD_STORAGE_BUCKET',
+  'CLOUD_CREDENTIALS_PATH',
+];
+
+const variablesSupplied = REQUIRED_VARIABLES.every(envVar => !!process.env[envVar]);
+
+if(!variablesSupplied)
   throw new Error('Required environment variables not provided');
 
 const DEFAULT_POSTGRES = 'postgresql://postgres:1234@localhost:5432/docs-bot';
@@ -14,6 +23,7 @@ const CLOUD_CONVERT_KEY = String(process.env.CLOUD_CONVERT_KEY);
 const PROJECT_ID = String(process.env.CLOUD_PROJECT_ID);
 const BUCKET = String(process.env.CLOUD_STORAGE_BUCKET);
 const HEALTH_SERVER = process.env.HEALTH_SERVER;
+const CREDENTIALS_FILENAME = process.env.CLOUD_CREDENTIALS_PATH;
 
 module.exports = {
   // Database
@@ -30,7 +40,7 @@ module.exports = {
   // Cloud Storage
   cloudStorage: {
     projectId: PROJECT_ID,
-    credentialsFilePath: path.resolve(__dirname, '..', 'credentials', 'admin.json'),
+    credentialsFilePath: path.resolve(__dirname, '..', 'credentials', CREDENTIALS_FILENAME),
     bucketName: BUCKET,
   },
 
